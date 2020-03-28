0482a2550561e102dfeda606b085732e62cca190
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-28 23:43:27 +0530

    root is set

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -6,3 +6,9 @@ import problem1.mybst.MyBinarySearchTree;
 public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree m = new MyBinarySearchTree();
+        Methods mthds = new Methods();
+
+        //setting up the root
+        m.setRoot();
+        System.out.println("Root set Successfully value :" + m.getRoot().getData());
+
