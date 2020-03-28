ed0c15502b967212329d8c526c41fad612feacde
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-28 18:16:28 +0530

    root is set

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,11 +1,12 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
 package problem1.main;
-// executable class
+
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
 
 public class MyMain {
-}
+    public static void main(String[] args) {
+        MyBinarySearchTree m = new MyBinarySearchTree();
+        MyQueue queue = new MyQueue();
+        //setting root
+        m.setRoot();
