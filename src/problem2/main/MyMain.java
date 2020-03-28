1c95a62ab37368ce3a5f1772277714e43376c6fe
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-28 23:50:52 +0530

    main function is completed

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -3,6 +3,7 @@ package problem2.main;
 import problem1.mybst.MyBinarySearchTree;
 
 
+
 public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree m = new MyBinarySearchTree();
@@ -11,6 +12,7 @@ public class MyMain {
         //setting up the root
         m.setRoot();
         System.out.println("Root set Successfully value :" + m.getRoot().getData());
+
 
         //Setting up the bst
         for (int i = 0; i < 4; i++) {
@@ -22,5 +24,6 @@ public class MyMain {
         mthds.verify_A(m.getRoot());
 
         System.out.println(mthds.verify_B(m.getRoot()));
-
+    }
+}
 
