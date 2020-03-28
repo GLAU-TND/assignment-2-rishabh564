0fc954c98cb06bece8e366c9e52a3e183e7cc5df
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-28 23:38:48 +0530

    MyBinarySearchTree object is made

---
+++ src/problem2/main/MyMain.java
@@ -0,0 +1,8 @@
+package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
+
+public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree m = new MyBinarySearchTree();
