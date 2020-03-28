848cdcf7ffd86441abd2b0d7d8de4883df97473c
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-28 18:43:41 +0530

    queue is printed

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -10,3 +10,23 @@ public class MyMain {
         MyQueue queue = new MyQueue();
         //setting root
         m.setRoot();
+        System.out.println("root set : " + m.getRoot().getData());
+
+
+        //insertion
+        for (int i = 0; i < 5; i++) {
+            m.insert(m.getRoot());
+        }
+
+
+        //printing left children
+        queue.queuePrint(m.getPre());
+        //counting nodes Not having left children
+        m.countNotLeft(m.getRoot());
+        //printing count
+        System.out.println(m.getCount());
+    }
+
+}
+
+
