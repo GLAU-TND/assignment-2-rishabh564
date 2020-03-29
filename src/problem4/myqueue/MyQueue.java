c0075c8610afd9a9ae979a306f4a544b1cdf88fa
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-29 17:00:07 +0530

    constructor is added

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -5,3 +5,26 @@ public class MyQueue {
 
     private Node front, end, tmp;
     private int size;
+
+    public MyQueue() {
+        front = null;
+        end = null;
+        tmp = null;
+        size = 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
