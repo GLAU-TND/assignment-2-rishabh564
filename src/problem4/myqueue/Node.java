bc6cefc0cd4d52b5ce75e76586c50a7f7ac5b5a9
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-29 17:59:57 +0530

    constructor is used

--- src/problem4/myqueue/Node.java
+++ src/problem4/myqueue/Node.java
@@ -6,4 +6,9 @@ public class MyQueue {
     private Node front, end, tmp;
     private int size;
 
-
+    public MyQueue() {
+        front = null;
+        end = null;
+        tmp = null;
+        size = 0;
+    }
