773c7fd8dfb390fd209f2ede006dd5fec3f4ea9a
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-29 21:03:07 +0530

    Queue is printed

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -1,4 +1,21 @@
 package problem5.main;
 
+import problem5.circularqueue.MyCircularQueue;
+
 //executable class
 public class MyMain {
+
+    {
+        public static void main (String[]args)
+        {
+            MyCircularQueue m = new MyCircularQueue();
+            Node node;
+
+            for (int i = 0; i < 5; i++) {
+                node = new Node(new Student());
+                m.enqueue(node);
+            }
+            m.printQueue();
+
+
+        }
