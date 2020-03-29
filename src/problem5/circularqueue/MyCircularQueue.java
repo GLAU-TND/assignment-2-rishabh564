b3caa565091b549a96e006a04e4001fc0d7bc314
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-29 20:54:51 +0530

    process function is used

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -71,6 +71,21 @@ public class MyCircularQueue {
         }
     }
 
+    public void process(String name) {
+        tmp = front;
+        if (tmp.getS().getName().equals(name)) {
+            System.out.println(tmp.getS().toString());
+            System.out.println(tmp.getS().getBacklog() - tmp.getS().getApperingcount());
+        }
+        while (!tmp.getS().getName().equals(name)) {
+            tmp = tmp.getNext();
+            if (tmp == front)
+                return;
+        }
+        System.out.println(tmp.getS().toString());
+        System.out.println(tmp.getS().getBacklog() - tmp.getS().getApperingcount());
+    }
+}
 
 
 
