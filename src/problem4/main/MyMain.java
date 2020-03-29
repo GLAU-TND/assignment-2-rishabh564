247602c1627adf7ffe5559c281043cbcc1867823
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-29 16:33:15 +0530

    root is set

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -1,2 +1,23 @@
 package problem4.main;
 
+import problem1.mybst.MyBinarySearchTree;
+import problem2.main.Methods;
+import problem4.myqueue.MyQueue;
+
+import java.util.Scanner;
+
+public class MyMain {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Methods mthds = new Methods();
+        MyBinarySearchTree m = new MyBinarySearchTree();
+        MyQueue q = new MyQueue();
+
+        //setting up the root
+        m.setRoot();
+
+        //inserting into the tree
+        for (int i = 0; i < 5; i++) {
+            m.insert(m.getRoot());
+        }
