77834b71d41faa72ab7d1a3d500a65c6fd5f9f3d
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-29 17:33:53 +0530

    enqueue function is used

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -61,6 +61,19 @@ public class MyQueue {
         queue.tmp = queue.front;
     }
 
+    public void enqueue(Node node) {
+
+        if (front == null) {
+            tmp = front = end = node;
+        } else {
+            while (tmp.getNext() != null) {
+                tmp = tmp.getNext();
+            }
+            end = node;
+            tmp.setNext(node);
+            tmp = front;
+        }
+    }
 
 
 
