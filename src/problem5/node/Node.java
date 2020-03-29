6e05e959fdd73eee0f4c71b456fb7da9567f0eca
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-29 21:06:01 +0530

    getS function is created

---
+++ src/problem5/node/Node.java
@@ -0,0 +1,13 @@
+package problem5.node;
+
+import problem5.student.Student;
+
+// to define node properties
+public class Node {
+    private Student s;
+    private Node next;
+
+
+    public Student getS() {
+        return s;
+    }
