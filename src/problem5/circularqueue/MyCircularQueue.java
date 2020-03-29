d6d7c1d3de0f673a2bd5777c6ee32f4da3431b68
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-29 20:49:09 +0530

    printQueue function is used

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -44,6 +44,37 @@ public class MyCircularQueue {
 
     }
 
+    public void printQueue() {
+        tmp = front;
+        try {
+            do {
+                System.out.println(tmp.getS().toString());
+                tmp = tmp.getNext();
+            }
+            while (tmp != front && tmp != null);
+        } catch (NullPointerException ignored) {
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
