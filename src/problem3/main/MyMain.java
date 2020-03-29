2224341ff713b2d877ed3de13f5ee0611cbb3dce
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-29 12:46:08 +0530

    main function is used

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -1,2 +1,40 @@
 package problem3.main;
 
+import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+
+public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue m = new MyPriorityQueue();
+        for (int i = 0; i < 4; i++) {
+            Node newNode = new Node();
+            m.enqueue(newNode);
+        }
+        m.dequeue();
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
