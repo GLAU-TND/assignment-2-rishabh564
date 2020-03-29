b69fab08ad8b54822ef4691bfe69a2d7dfdcaeb6
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-29 12:55:50 +0530

    to get next Node

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -7,3 +7,16 @@ public class MyPriorityQueue {
 
     private Node tmp, front, end;
     private int i = 0;
+
+    //inserting  new student into into queue  and priority is set by roll number
+    public void enqueue(Node newNode) {
+
+        if (front == null || newNode.getS().getRollno() < front.getS().getRollno()) {
+            newNode.setNext(front);
+            front = newNode;
+        } else {
+            tmp = front;
+
+            while (tmp.getNext() != null && tmp.getNext().getS().getRollno() < newNode.getS().getRollno()) {
+                tmp = tmp.getNext();
+            }
